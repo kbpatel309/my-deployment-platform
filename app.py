68134768d83354338d0d78f7490fd61ac49ea738
Kishan Patel <kbpatel309@gmail.com>
2025-11-25 23:57:09 -0600

    readded deploy endpoint

--- app.py
+++ app.py
@@ -1,22 +1,30 @@
 # Import the Flask class from the flask package
-
+from flask import Flask, render_template, request, jsonify
 
 # Create an instance of the Flask class
-
+app = Flask(__name__)
 
 # Define a route for the root URL
+@app.route('/')
 
 # Define a view function associated with the route
+def home():
+    return render_template('index.html')
 
-
-
+@app.route('/deploy', methods=['POST'])
+def deploy():
     # Get data sent from the form
-    
+    data = request.get_json()
+    github_url = data.get('github_url')
 
     # For now, just send it back
-    
+    return jsonify({
+        'status': 'received',
+        'message': f'Got your URL: {github_url}'
+    })
 
 # Check if the script is run directly
-
+if __name__ == '__main__':
     # Run the Flask application
+    app.run(debug=True, port=5000)
     
